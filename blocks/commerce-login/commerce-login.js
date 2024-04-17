e73d081da46236369decc003fcf9123eee75ed2d
Chris Vander Poel <chris.vanderpoel@gmail.com>
2024-04-17 10:20:31 -0400

    Initialize auth-dropin

---
+++ blocks/commerce-login/commerce-login.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
+import { SignIn } from '@dropins/storefront-auth/containers/SignIn.js';
+import { SuccessNotification } from '@dropins/storefront-auth/containers/SuccessNotification.js';
+import { authLogoutService } from '@dropins/storefront-auth/service/authLogoutService.js';
+import { render as authRenderer } from '@dropins/storefront-auth/render.js';
+import { getCookie } from '../../scripts/configs.js';
+import { h } from '../../scripts/preact.js';
+
+export default function decorate(block) {
+  const isAuthenticated = !!getCookie('auth_dropin_user_token');
+
+  if (isAuthenticated) {
+    window.location.href = '/customer/account';
+  } else {
+    authRenderer.render(SignIn, {
+      formSize: 'default',
+      forgotPasswordPageRedirectUrl: '/customer/forgotpassword',
+      successNotificationForm: (userName) => h(SuccessNotification, {
+        formSize: 'default',
+        className: 'initClass',
+        headingText: `Welcome ${userName}`,
+        messageText: 'Your account has been successfully created.',
+        primaryButtonText: 'My Account',
+        secondaryButtonText: 'Logout',
+        onPrimaryButtonClick: () => {
+          window.location.href = '/customer/account';
+        },
+        onSecondaryButtonClick: async () => {
+          await authLogoutService();
+          window.location.href = '/customer/login';
+        },
+      }),
+    })(block);
+  }
+}
